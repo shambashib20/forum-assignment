821a9610d81412f247a2d7ed739cb6ffba6dc7b6
shambashib20 <shambashibmajumdar2000@gmail.com>
2024-05-28 20:05:06 +0530

    some updates

--- src/pages/home-page/home-page/index.tsx
+++ src/pages/home-page/home-page/index.tsx
@@ -1,16 +1,148 @@
 import { FC } from "react";
+import { NavBar } from "@/components/nav-bar";
 
 const HomePage: FC = () => {
 
     return (
-    //     <h1 className="text-2xl ">
+      <>
+      <NavBar />
+       <section className="
+           h-screen
+           max-w-7xl mx-auto px-4 sm:px-6
+        ">
+          
+        <h1 className="
+          font-sans
+          font-bold
+        ">Home Page</h1>  
+
+<div className="flex flex-col gap-4 ">
+  
+</div>
+        {/* <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1523978591478-c753949ff840?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1497398276231-94ff5dc90217?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My much longer heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1503863937795-62954a3c0f05?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Shorter text.</p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1511084901824-1c57f5a16c98?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Shorter text.</p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1525935944571-4e99237764c9?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
+          <div
+            className="bg-cover h-48"
+            style={{ backgroundImage: "url(https://images.unsplash.com/photo-1486506574467-c44963fc7876?w=900)" }}
+          ></div>
+          <div className="p-4 flex-1 flex flex-col">
+            <h3 className="mb-4 text-2xl">My heading</h3>
+            <div className="mb-4 text-grey-darker text-sm flex-1">
+              <p>Longer content.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+            </div>
+            <a href="#" className="border-t border-grey-light pt-2 text-xs text-grey hover:text-red uppercase no-underline tracking-wide">
+              Twitter
+            </a>
+          </div>
+        </div>
+      </div>  */}
+      </section>
+
+      
+      {/* <section
+         classNameName="max-w-7xl mx-auto px-4 sm:px-6 h-screen"
+      >
+
+      </section>
+       */}
+      </>
+    //     <h1 classNameName="text-2xl ">
     //   Hello world!
     // </h1>
-        <section className="flex justify-center items-center h-screen">
-        <h1 className="text-center  font-bold">
-          Home Page
-        </h1>
-      </section>
+    
+       
     )
 
 };
